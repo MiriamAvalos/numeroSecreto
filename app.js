1a9dfb4b0e201b881ebdd9822b7043bbdf75ad75
Mirisaurio <Mirisaurio>
2024-03-04 20:13:44 -0600

    primer commit

---
+++ app.js
@@ -0,0 +1,9 @@
+//accedo al elemento(objeto) del DOM por medio de su selector y le asigno su valor a una variable para poder acceder a el
+let titulo = document.querySelector("h1");
+//le asigno el valor al objeto mediante el metodo innerHTML
+titulo.innerHTML= "Juego del número secreto"
+
+let parrafo = document.querySelector("p");
+parrafo.innerHTML= "Indica un número del 1 al 10"
+
+
