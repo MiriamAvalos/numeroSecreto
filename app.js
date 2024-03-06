3442168acab24e482a13f480d1f47d5c9a6f8df6
Mirisaurio <Mirisaurio>
2024-03-05 20:46:10 -0600

    se añaden mensajes al usuario si acerto o el numero es mayor o menor

--- app.js
+++ app.js
@@ -4,14 +4,14 @@ let numeroSecreto = generarNumeroSecreto();
 //funcion para acceder al elemento(objeto) del DOM por medio de su selector y asignar su valor a una variable para poder cambiar su valor
 //le asigno el valor al objeto mediante el metodo innerHTML
 function asignarTextoElemento(elemento, texto) {
-  let elementoHTML = document.querySelector(elemento);
+  let elementoHTML = document.querySelector(elemento); 
   elementoHTML.innerHTML = texto;
   //esta función no retorna nada, solo asigna un valor al elemento HTML, pero es buena practica siempre poner return a las funciones.
   return;
 }
 //llamamos a la funcion y se le asignan los valores a los parametros para cambiar el texto
 asignarTextoElemento('h1', 'Juego del número secreto');
-asignarTextoElemento('p', 'Indica un número del 1 al 100');
+asignarTextoElemento('p', 'Indica un número del 1 al 10');
 
 //función para generar el número secreto
 function generarNumeroSecreto() {
@@ -21,12 +21,20 @@ function generarNumeroSecreto() {
 //función para obtener el número que el usuario ingresa en el input
 function verificarIntento() {
   let numeroDeUsuario = parseInt(document.getElementById('valorUsuario').value);
-  console.log(typeof(numeroDeUsuario));
-  console.log(typeof(numeroSecreto));
+  //console.log(typeof(numeroDeUsuario));
+  //console.log(typeof(numeroSecreto));
   console.log(numeroSecreto);
-  console.log(numeroDeUsuario);
-  console.log(numeroSecreto === numeroDeUsuario);
+  //console.log(numeroDeUsuario);
+  //console.log(numeroSecreto === numeroDeUsuario);
+  if(numeroSecreto === numeroDeUsuario){
+   asignarTextoElemento('p','¡Felicidades! Acertaste el número.');
+  } else {
+    if(numeroDeUsuario > numeroSecreto){
+      asignarTextoElemento('p','El número secreto es menor');
+    } else {
+      asignarTextoElemento('p','El número secreto es mayor');
+    }
+  }
   return;
 }
-
 
